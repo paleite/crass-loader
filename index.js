96418766d7472ac44ffcbcabbc4782bb3f949d84
Patrick Eriksson <paleite@kth.se>
2017-09-25 15:21:16 +0200

    Added linting and formatting

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
-"use strict";
+const crass = require(`crass`)
+const browserSupport = require(`crass/dist/browser_support`)
+const loaderUtils = require(`loader-utils`)
 
-var crass = require("crass");
-var browser_support = require("crass/dist/browser_support");
-var loaderUtils = require("loader-utils");
-
-module.exports = function(source) {
-  var crassOptions = loaderUtils.getOptions(this);
-  var options = {
+module.exports = function (source) {
+  const crassOptions = loaderUtils.getOptions(this)
+  const options = {
     optimize: true
-  };
-  var result;
+  }
+  let result
 
   if (this.cacheable) {
-    this.cacheable();
+    this.cacheable()
   }
 
-  if (crassOptions !== null && typeof crassOptions === "object") {
-    if (typeof crassOptions.optimize === "boolean") {
-      options.optimize = crassOptions.optimize;
+  if (crassOptions !== null && typeof crassOptions === `object`) {
+    if (typeof crassOptions.optimize === `boolean`) {
+      options.optimize = crassOptions.optimize
     }
-    if (typeof crassOptions.o1 === "boolean") {
-      options.o1 = crassOptions.o1;
+    if (typeof crassOptions.o1 === `boolean`) {
+      options.o1 = crassOptions.o1
     }
-    if (typeof crassOptions.pretty === "boolean") {
-      options.pretty = crassOptions.pretty;
+    if (typeof crassOptions.pretty === `boolean`) {
+      options.pretty = crassOptions.pretty
     }
-    if (typeof crassOptions.saveie === "boolean") {
-      options.saveie = crassOptions.saveie;
+    if (typeof crassOptions.saveie === `boolean`) {
+      options.saveie = crassOptions.saveie
     }
-    if ("min" in crassOptions) {
-      var minPlatsUnparsed = crassOptions.min.split(",");
+    if (`min` in crassOptions) {
+      const minPlatsUnparsed = crassOptions.min.split(`,`)
       if (minPlatsUnparsed.length) {
-        var minPlats = minPlatsUnparsed.map(browser_support.parseBrowser);
-        options.browser_min = {};
-        minPlats.forEach(function(plat) {
-          options.browser_min[plat.browser] = plat.version;
-        });
+        const minPlats = minPlatsUnparsed.map(browserSupport.parseBrowser)
+        options.browser_min = {}
+        minPlats.forEach(plat => {
+          options.browser_min[plat.browser] = plat.version
+        })
       }
     } else {
       options.browser_min = {
@@ -43,29 +41,29 @@ module.exports = function(source) {
         firefox: 31,
         ie: 11,
         opera: 26
-      };
+      }
     }
 
-    if (typeof crassOptions.css4 === "boolean") {
-      options.css4 = crassOptions.css4;
+    if (typeof crassOptions.css4 === `boolean`) {
+      options.css4 = crassOptions.css4
     }
   }
 
   if (options.optimize) {
     try {
-      result = crass.parse(source).optimize(options);
+      result = crass.parse(source).optimize(options)
     } catch (error) {
-      result = null;
+      result = null
     }
   }
 
   if (result) {
     if (options.pretty) {
-      return result.pretty();
+      return result.pretty()
     } else {
-      return result.toString();
+      return result.toString()
     }
   }
 
-  return source;
-};
+  return source
+}
--- test.js
+++ test.js
@@ -1,81 +1,81 @@
-const loader = require("./index.js");
-const invalidInput = "div { background=white; }";
-const simpleInput = "div { background: white; }";
-const simpleOutput = "div{background:#fff}";
-const prettyOutput = "div {\n  background: #fff;\n}\n";
-const saveieInput = "a{foo:bar\\9}";
-const saveieOutput = "a{foo:bar\\9}";
-const browserSupportInput = "a{-moz-border-radius:0;foo:bar}";
-const browserSupportOutput = "a{foo:bar}";
-const o1Input = "@-webkit-keyframes foo{0%{a:b}}@keyframes foo{0%{-webkit-transform:a;transform:b}}";
-const o1Output = "@-webkit-keyframes foo{0%{a:b}}@keyframes foo{0%{transform:b}}";
-const css4Input = "b{color:rgba(255,255,255,0.5)}";
-const css4Output = "b{color:gray(100%/.5)}";
+const loader = require(`./index.js`)
+const invalidInput = `div { background=white; }`
+const simpleInput = `div { background: white; }`
+const simpleOutput = `div{background:#fff}`
+const prettyOutput = `div {\n  background: #fff;\n}\n`
+const saveieInput = `a{foo:bar\\9}`
+const saveieOutput = `a{foo:bar\\9}`
+const browserSupportInput = `a{-moz-border-radius:0;foo:bar}`
+const browserSupportOutput = `a{foo:bar}`
+const o1Input = `@-webkit-keyframes foo{0%{a:b}}@keyframes foo{0%{-webkit-transform:a;transform:b}}`
+const o1Output = `@-webkit-keyframes foo{0%{a:b}}@keyframes foo{0%{transform:b}}`
+const css4Input = `b{color:rgba(255,255,255,0.5)}`
+const css4Output = `b{color:gray(100%/.5)}`
 
-let globalOutput = [];
-console.error = function(message) {
-  globalOutput.push(message);
-};
+let globalOutput = []
+console.error = function (message) {
+  globalOutput.push(message)
+}
 
-let testLoader = function(input, output, options) {
-  options = typeof options !== "undefined" ? options : {};
-  expect(loader.call(options, input)).toEqual(output);
-};
+let testLoader = function (input, output, options) {
+  options = typeof options !== `undefined` ? options : {}
+  expect(loader.call(options, input)).toEqual(output)
+}
 
-describe("crass-loader", function() {
-  it("should minimize css", function() {
-    testLoader(simpleInput, simpleOutput);
-  });
+describe(`crass-loader`, () => {
+  it(`should minimize css`, () => {
+    testLoader(simpleInput, simpleOutput)
+  })
 
-  it("should return original css in case of error input", function() {
-    testLoader(invalidInput, invalidInput);
-  });
+  it(`should return original css in case of error input`, () => {
+    testLoader(invalidInput, invalidInput)
+  })
 
-  it("should return unoptimized result due to false in optimize in crass options", function() {
+  it(`should return unoptimized result due to false in optimize in crass options`, () => {
     testLoader(simpleInput, simpleInput, {
       query: {
         optimize: false
       }
-    });
-  });
+    })
+  })
 
-  it("should return o1 optimized result due to true in o1 in crass options", function() {
+  it(`should return o1 optimized result due to true in o1 in crass options`, () => {
     testLoader(o1Input, o1Output, {
       query: {
         o1: true
       }
-    });
-  });
+    })
+  })
 
-  it("should return prettified result due to true in pretty in crass options", function() {
+  it(`should return prettified result due to true in pretty in crass options`, () => {
     testLoader(simpleInput, prettyOutput, {
       query: {
         pretty: true
       }
-    });
-  });
+    })
+  })
 
-  it("should return ie6 features due to true in saveie in crass options", function() {
+  it(`should return ie6 features due to true in saveie in crass options`, () => {
     testLoader(saveieInput, saveieOutput, {
       query: {
         saveie: true
       }
-    });
-  });
+    })
+  })
 
-  it("should return browser optimized result due to parameters in min in crass options", function() {
+  it(`should return browser optimized result due to parameters in min in crass options`, () => {
     testLoader(browserSupportInput, browserSupportOutput, {
       query: {
-        min: "ie1,fx5,chr1"
+        min: `ie1,fx5,chr1`
       }
-    });
-  });
+    })
+  })
 
-  it("should return optimized css4 result due to true in css4 in crass options", function() {
+  it(`should return optimized css4 result due to true in css4 in crass options`, () => {
     testLoader(css4Input, css4Output, {
       query: {
         css4: true
       }
-    });
-  });
-});
+    })
+  })
+})
